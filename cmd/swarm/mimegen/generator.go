f2b2d06a42b5a5304900a390dc51767668a545ca
Andrey Galkin <andrey@futoin.org>
2019-06-05 21:07:40 +0300

    CHANGED: result of automatic update-license run

--- cmd/swarm/mimegen/generator.go
+++ cmd/swarm/mimegen/generator.go
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 // Standard "mime" package rely on system-settings, see mime.osInitMime
