46d5eb0958cc60b4c6035bc379aaeb1969e24d54
Andrey Galkin <andrey@futoin.org>
2019-06-15 06:50:50 +0300

    NEW: Genesis-time EVM execution for contract creation at fixed address

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -454,6 +454,11 @@ func (evm *EVM) Create(caller ContractRef, code []byte, gas uint64, value *big.I
 	return evm.create(caller, &codeAndHash{code: code}, gas, value, contractAddr)
 }
 
+// Create creates a new contract using code as deployment code.
+func (evm *EVM) CreateGenesis(caller ContractRef, contractAddr common.Address, code []byte, gas uint64, value *big.Int) (ret []byte, _ common.Address, leftOverGas uint64, err error) {
+	return evm.create(caller, &codeAndHash{code: code}, gas, value, contractAddr)
+}
+
 // Create2 creates a new contract using code as deployment code.
 //
 // The different between Create2 with Create is Create2 uses sha3(0xff ++ msg.sender ++ salt ++ sha3(init_code))[12:]
