53270cf483797beed6e254d447f7cb9b37f43e03
Andrey Galkin <andrey@futoin.org>
2019-06-28 19:12:41 +0300

    NEW: Energi Testnet bootnodes

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -22,7 +22,10 @@ var MainnetBootnodes = []string{}
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // Ropsten test network.
-var TestnetBootnodes = []string{}
+var TestnetBootnodes = []string{
+	"enode://ccb54af42913ed8fa23b890c26f13e305057f8036aab94dd6e884da26c5853fe60079edf6a180cee72f395602c18bd115bbc06e41353d62589db52f26ac25964@34.211.246.66:49797",
+	"enode://439da3ce5873d7afb3f4516ecc8bf3fd3ff3772f00fdbe9248d13978ab9f2e5a95aeb20e808ef5248ca5e071853202c0bdaf9aae8c5bf907423de5d575f34665@52.36.22.29:49797",
+}
 
 // RinkebyBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // Rinkeby test network.
