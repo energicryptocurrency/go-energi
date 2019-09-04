90d0683c0e7fd1f262baf8ec31f54873c672de64
Andrey Galkin <andrey@futoin.org>
2019-09-04 04:19:17 +0300

    NEW: SC-17 - consolidated BlockReward contract

---
+++ energi/contracts/src/BlockRewardV1.sol
@@ -0,0 +1,88 @@
+// Copyright 2019 The Energi Core Authors
+// This file is part of Energi Core.
+//
+// Energi Core is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Energi Core is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Energi Core. If not, see <http://www.gnu.org/licenses/>.
+
+// Energi Governance system is the fundamental part of Energi Core.
+
+// NOTE: It's not allowed to change the compiler due to byte-to-byte
+//       match requirement.
+pragma solidity 0.5.11;
+//pragma experimental SMTChecker;
+
+import { GovernedContract } from "./GovernedContract.sol";
+import { IBlockReward } from "./IBlockReward.sol";
+import { IGovernedProxy } from "./IGovernedProxy.sol";
+import { NonReentrant } from "./NonReentrant.sol";
+
+/**
+ * Genesis hardcoded version of SporkReward
+ *
+ * NOTE: it MUST NOT change after blockchain launch!
+ */
+contract BlockRewardV1 is
+    GovernedContract,
+    IBlockReward,
+    NonReentrant
+{
+    uint constant internal GET_GAS = 10000;
+
+    IGovernedProxy[] public reward_proxies;
+
+    // IGovernedContract
+    //---------------------------------
+    constructor(address _proxy, IGovernedProxy[] memory _reward_proxies)
+        public
+        GovernedContract(_proxy)
+    {
+        for (uint i = 0; i < _reward_proxies.length; ++i) {
+            reward_proxies.push(_reward_proxies[i]);
+        }
+    }
+
+    // IBlockReward
+    //---------------------------------
+    function reward()
+        external payable
+        noReentry
+    {
+        uint len = reward_proxies.length;
+        uint gas_per_reward = (gasleft() / len) - GET_GAS;
+
+        for (uint i = 0; i < len; ++i) {
+            IBlockReward impl = IBlockReward(address(reward_proxies[i].impl()));
+
+            uint amount = impl.getReward.gas(GET_GAS)(block.number);
+
+            // solium-disable-next-line security/no-call-value
+            address(impl).call.value(amount).gas(gas_per_reward)(abi.encode(impl.reward.selector));
+        }
+    }
+
+    function getReward(uint _blockNumber)
+        external view
+        returns(uint amount)
+    {
+        for (uint i = reward_proxies.length; i-- > 0;) {
+            IBlockReward impl = IBlockReward(address(reward_proxies[i].impl()));
+            amount += impl.getReward(_blockNumber);
+        }
+    }
+
+    // Safety
+    //---------------------------------
+    function () external payable {
+        revert("Not supported");
+    }
+}
---
+++ energi/contracts/test/BlockRewardV1.spec.js
@@ -0,0 +1,76 @@
+// Copyright 2019 The Energi Core Authors
+// This file is part of Energi Core.
+//
+// Energi Core is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Energi Core is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Energi Core. If not, see <http://www.gnu.org/licenses/>.
+
+// Energi Governance system is the fundamental part of Energi Core.
+
+'use strict';
+
+const MockProxy = artifacts.require('MockProxy');
+const MockContract = artifacts.require('MockContract');
+const BlockRewardV1 = artifacts.require('BlockRewardV1');
+const IBlockReward = artifacts.require('IBlockReward');
+
+const common = require('./common');
+
+contract("BlockRewardV1", async accounts => {
+    const s = {
+        artifacts,
+        accounts,
+        assert,
+        it,
+        web3,
+        backbone : accounts[5],
+    };
+
+    before(async () => {
+        s.orig = await BlockRewardV1.deployed();
+        s.proxy = await MockProxy.at(await s.orig.proxy());
+        s.fake = await MockContract.new(s.proxy.address);
+        s.proxy_abi = await BlockRewardV1.at(s.proxy.address);
+        s.token_abi = await IBlockReward.at(s.proxy.address);
+        await s.proxy.setImpl(s.orig.address);
+        Object.freeze(s);
+    });
+
+    describe('common pre', () => common.govPreTests(s) );
+
+    //---
+    describe('Primary', () => {
+        const { toBN, toWei } = web3.utils;
+        const backbone_reward = toBN(toWei('2.28', 'ether'));
+        const normal_reward = toBN(toWei('13.7', 'ether'));
+        const superblock_reward = toBN(toWei('184004.56', 'ether'));
+
+        it('should correctly getReward()', async () => {
+            expect(toBN(await s.token_abi.getReward(0)).toString())
+                .equal(toBN(0).toString());
+            expect(toBN(await s.token_abi.getReward(1)).toString())
+                .equal(toBN(normal_reward).toString());
+            expect(toBN(await s.token_abi.getReward(common.superblock_cycles)).toString())
+                .equal(toBN(superblock_reward).toString());
+        });
+
+        it('should reward()', async () => {
+            const bal_before = toBN(await web3.eth.getBalance(s.backbone));
+            await s.token_abi.reward({value: superblock_reward});
+            const bal_after = toBN(await web3.eth.getBalance(s.backbone));
+            expect(bal_after.sub(bal_before).toString()).equal(backbone_reward.toString());
+        });
+    });
+
+    //---
+    describe('common post', () => common.govPostTests(s) );
+});
