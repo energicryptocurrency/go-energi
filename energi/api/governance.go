6eeb9847068fcb8b1b8e5bd0163b917cb94ed719
Andrey Galkin <andrey@futoin.org>
2020-01-26 07:57:00 +0200

    CHANGED: updated copyright headers

--- energi/api/governance.go
+++ energi/api/governance.go
@@ -1,3 +1,6 @@
+// Copyright 2019 The Energi Core Authors
+// This file is part of the Energi Core library.
+//
 // The Energi Core library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
