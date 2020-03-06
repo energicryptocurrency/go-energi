0c9077508501433e066f6b75570004c0f658ff2a
Andrey Galkin <andrey@futoin.org>
2020-03-07 00:20:44 +0200

    CHANGED: marked source as stable

--- params/version.go
+++ params/version.go
@@ -25,7 +25,7 @@ const (
 	VersionMajor = 0       // Major version component of the current release
 	VersionMinor = 9       // Minor version component of the current release
 	VersionPatch = 7       // Patch version component of the current release
-	VersionMeta  = "devel" // Version metadata to append to the version string
+	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
