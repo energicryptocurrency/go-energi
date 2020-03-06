4ca5b6549d7547c95508e83384b7ed1f8587a460
Andrey Galkin <andrey@futoin.org>
2020-03-07 00:21:06 +0200

    Updated for release energi3 3.0.0

--- params/version.go
+++ params/version.go
@@ -22,9 +22,9 @@ import (
 )
 
 const (
-	VersionMajor = 0       // Major version component of the current release
-	VersionMinor = 9       // Minor version component of the current release
-	VersionPatch = 7       // Patch version component of the current release
+	VersionMajor = 3       // Major version component of the current release
+	VersionMinor = 0       // Minor version component of the current release
+	VersionPatch = 0       // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
