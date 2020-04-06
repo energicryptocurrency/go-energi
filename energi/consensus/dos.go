b4717929de7e2a2bc9ec3dc3d1f13b8c7b8c7918
Ryan Lucchese <ryan@energi.team>
2020-04-06 10:05:20 -0600

    FIXED: disabled POS-8

--- energi/consensus/dos.go
+++ energi/consensus/dos.go
@@ -51,17 +51,18 @@ func (e *Energi) checkDoS(
 	header *types.Header,
 	parent *types.Header,
 ) error {
-	old_fork_threshold := e.now() - energi_params.OldForkPeriod
-
-	// POS-8: allow old fork only if current head is not fresh enough
-	//---
-	if parent.Time < old_fork_threshold {
-		current := chain.CurrentHeader()
-
-		if current.Time > old_fork_threshold {
-			return eth_consensus.ErrDoSThrottle
-		}
-	}
+	// POS-8 is disabled due to issues with chain splits
+//	old_fork_threshold := e.now() - energi_params.OldForkPeriod
+//
+//	// POS-8: allow old fork only if current head is not fresh enough
+//	//---
+//	if parent.Time < old_fork_threshold {
+//		current := chain.CurrentHeader()
+//
+//		if current.Time > old_fork_threshold {
+//			return eth_consensus.ErrDoSThrottle
+//		}
+//	}
 
 	// POS-9: stake throttling
 	//---
