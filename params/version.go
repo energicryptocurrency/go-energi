58659bd5be88d0ae9b53606a6cb733d732444e8a
Ryan Lucchese <ryan@energi.team>
2020-04-24 18:41:36 -0600

    update version to v3.0.6

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ import (
 const (
 	VersionMajor = 3       // Major version component of the current release
 	VersionMinor = 0       // Minor version component of the current release
-	VersionPatch = 5       // Patch version component of the current release
+	VersionPatch = 6       // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
