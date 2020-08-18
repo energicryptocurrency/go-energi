be6b056f33f2a059c875b6999d10a079efcc47cd
giou-k <konstantinos.georgiou@energi.team>
2020-08-18 16:05:23 +0300

    FIXED: #4685 - Restrict the port numbers that simnet and testnet can accept.

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -801,12 +801,18 @@ func setBootstrapNodesV5(ctx *cli.Context, cfg *p2p.Config) {
 // setListenAddress creates a TCP listening address string from set command
 // line flags.
 func setListenAddress(ctx *cli.Context, cfg *p2p.Config) {
-	if ctx.GlobalIsSet(ListenPortFlag.Name) {
-		cfg.ListenAddr = fmt.Sprintf(":%d", ctx.GlobalInt(ListenPortFlag.Name))
-	} else if ctx.GlobalIsSet(TestnetFlag.Name) {
-		cfg.ListenAddr = fmt.Sprintf(":%d", 49797)
+	if ctx.GlobalIsSet(TestnetFlag.Name) {
+		if ctx.GlobalInt64(ListenPortFlag.Name) != 49797 {
+			log.Error("Unacceptable port value. Testnet port is being set to 49797.")
+			cfg.ListenAddr = fmt.Sprintf(":%d", 49797)
+		}
 	} else if ctx.GlobalIsSet(SimnetFlag.Name) {
-		cfg.ListenAddr = fmt.Sprintf(":%d", 59797)
+		if ctx.GlobalInt64(ListenPortFlag.Name) != 59797 {
+			log.Error("Unacceptable port value. Simnet port is being set to 59797.")
+			cfg.ListenAddr = fmt.Sprintf(":%d", 59797)
+		}
+	} else if ctx.GlobalIsSet(ListenPortFlag.Name) {
+		cfg.ListenAddr = fmt.Sprintf(":%d", ctx.GlobalInt(ListenPortFlag.Name))
 	}
 }
 
