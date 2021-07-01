82b7561e355e67ae183fd21b7dd5c56458940977
David Vennik <david.vennik@energi.team>
2021-07-01 09:39:13 +0300

    adding some comments, tidying up whitespacing

--- energi/params/consensus.go
+++ energi/params/consensus.go
@@ -19,63 +19,64 @@ package params
 type ctxKey string
 
 const (
-	MaturityPeriod    uint64 = 60 * 60
-	AverageTimeBlocks uint64 = 60
-	TargetBlockGap    uint64 = 60
-	MinBlockGap       uint64 = 30
-	MaxFutureGap      uint64 = 3
-	TargetPeriodGap   uint64 = AverageTimeBlocks * TargetBlockGap
-	
+	MaturityPeriod  uint64 = 60 * 60
+	AveragingWindow uint64 = 60 // 60 blocks
+	TargetBlockGap  uint64 = 60 // 60 seconds after the target is max diff
+	MinBlockGap     uint64 = 30 // 30 seconds minimum before new timestamp
+	MaxFutureGap    uint64 = 3  // only accept blocks this many seconds ahead
+	// (subjective time)
+	TargetPeriodGap uint64 = AveragingWindow * TargetBlockGap
+
 	// DoS protection
 	OldForkPeriod uint64 = 15 * 60
 	StakeThrottle uint64 = 60
-	
-	UnlimitedGas uint64 = (1 << 40)
-	
+
+	UnlimitedGas uint64 = 1 << 40
+
 	MasternodeCallGas uint64 = 1000000
-	
-	// TargetWindow defines the number of blocks within the AverageTimeBlocks that
+
+	// TargetWindow defines the number of blocks within the AveragingWindow that
 	// will be averaged when calculating the simple moving average.
 	TargetWindow uint64 = 5
-	
+
 	// MaxCheckpointVoteBlockAge defines the period in blocks count from the time
 	// the checkpoint signer account proposes a checkpoint in which its voting
 	// is permitted.
 	MaxCheckpointVoteBlockAge = 1440
-	
+
 	// GeneralProxyCtxKey is used to pass the governed proxy address hash to
 	// the filter logs interface.
 	GeneralProxyCtxKey = ctxKey("governedProxyAddressHash")
-	
+
 	// NB: Time difference between the block target and new block time is always
 	// calulated as (blockTargetTime - newBlockTime).
-	
+
 	// MaxTimeDifferenceDrop defines the maximum time difference that can be used
 	// to calculate the difficulty drop when the newly created block is found long
 	// after block target time. This
 	MaxTimeDifferenceDrop = -30
-	
+
 	// DifficultyChangeBase defines the base constant that is used generate the
 	// difficulty multiplier. i.e.
 	// Time difference :   Difficulty Multiplier
 	//  +23            =>  1.0001^23 = 1.00230253177
 	//  -23            =>  1.0001^-23 = 0.9977027577
-	
+
 	// Maximum positive time difference will never exceed 60 (TargetBlockGap)
 	// and Minimum negative time difference will never exceed -30 (MaxTimeDifferenceDrop)
 	// Therefore:
-	
+
 	// Max difficulty rise multiplier => 1.0001^60 = 1.00601773427 (increases to 100.6%)
 	// Min difficulty drop multiplier => 1.0001^-30 = 0.9977027577 (drops to 99.77%)
 	DifficultyChangeBase = 1.0001
-	
+
 	// DiffV2MigrationStakerTimeDelay roughly get 2x difficulty decrease
 	DiffV2MigrationStakerTimeDelay uint64 = 15
-	
+
 	// DiffV2MigrationStakerBlockDelay defines block past which the migration staker
 	// difficulty decrease is no longer enforced.
 	DiffV2MigrationStakerBlockDelay uint64 = 10
-	
+
 	// DiffV2MigrationStakerTarget defines the maximum difficulty that the parent block
 	// to what the migration staker is mining results to time decrease.
 	DiffV2MigrationStakerTarget uint64 = 0xFFFF
