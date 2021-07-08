2cf7c7ca1330702f3554f06fe662f4c04f25520e
David Vennik <david.vennik@energi.team>
2021-07-08 07:23:21 +0300

    added a constant for the number of seconds range

--- energi/consensus/intervalgen/main.go
+++ energi/consensus/intervalgen/main.go
@@ -7,18 +7,18 @@ import (
 )
 
 func main() {
-	const  sampleNum =  60
+	const  sampleNum, nSeconds =  60, 60
 	// This will generate a consistent set of random values
 	rand.Seed(32)
 	// // This will generate an always changing set of random values
 	// rand.Seed(time.Now().UnixNano())
 	// create 60 samples as per needed for the interval test
 	samples := make([]uint64, sampleNum)
-	samples[0] = uint64(30 + rand.Int63n(60))
+	samples[0] = uint64(30 + rand.Int63n(nSeconds))
 	for i := range samples {
 		if i > 0 {
-			samples[i] = samples[i-1] + uint64(30+rand.Int63n(
-				60))
+			samples[i] = samples[i-1] +
+				uint64(30+rand.Int63n(nSeconds))
 		}
 	}
 	output := `package consensus
