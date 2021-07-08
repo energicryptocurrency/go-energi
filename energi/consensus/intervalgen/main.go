5532245d29e174d9876671422bb92dd0bb24f1f2
David Vennik <david.vennik@energi.team>
2021-07-08 07:28:43 +0300

    using the constants the previously used numbers refer to

--- energi/consensus/intervalgen/main.go
+++ energi/consensus/intervalgen/main.go
@@ -4,32 +4,36 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math/rand"
+
+	"energi.world/core/gen3/energi/params"
 )
 
 func main() {
-	const  sampleNum, nSeconds =  60, 60
+	const sampleNum = 60
 	// This will generate a consistent set of random values
 	rand.Seed(32)
 	// // This will generate an always changing set of random values
-	// rand.Seed(time.Now().UnixNano())
-	// create 60 samples as per needed for the interval test
+	// rand.Seed(time.Now().UnixNano()) which will have a typical average
+	// around the same as params.TargetBlockGap (60 second block interval)
 	samples := make([]uint64, sampleNum)
-	samples[0] = uint64(30 + rand.Int63n(nSeconds))
+	samples[0] = uint64(int64(params.MinBlockGap) +
+		rand.Int63n(int64(params.TargetBlockGap)))
 	for i := range samples {
 		if i > 0 {
 			samples[i] = samples[i-1] +
-				uint64(30+rand.Int63n(nSeconds))
+				uint64(int64(params.MinBlockGap)+
+					rand.Int63n(int64(params.TargetBlockGap)))
 		}
 	}
 	output := `package consensus
 	
-`+`//go:generate go run ./intervalgen/.
+` + `//go:generate go run ./intervalgen/.
 
 var emaSamples = []uint64{
 `
 	for i := range samples {
-		output += fmt.Sprint("\t",samples[i], ",\n")
+		output += fmt.Sprint("\t", samples[i], ",\n")
 	}
-	output+="}\n"
+	output += "}\n"
 	ioutil.WriteFile("posv2emasamples.go", []byte(output), 0660)
 }
