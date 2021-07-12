38b6c29952e776eca5c5c5454af39e70fe48f7fc
Ryan Lucchese <ryan@energi.team>
2021-07-12 08:52:54 -0600

    removing unneeded code and unifying time target type for difficulty v1 and difficulty v2

--- energi/consensus/posv2.go
+++ energi/consensus/posv2.go
@@ -18,13 +18,8 @@ package consensus
 
 import (
 	"math/big"
-	"sort"
-	"time"
 
-	"energi.world/core/gen3/common"
-	eth_consensus "energi.world/core/gen3/consensus"
 	"energi.world/core/gen3/core/types"
-	"energi.world/core/gen3/energi/diffv1"
 	"energi.world/core/gen3/energi/params"
 	"energi.world/core/gen3/log"
 )
@@ -38,54 +33,6 @@ const (
 	diffV2MigrationStakerBlockDelay = params.DiffV2MigrationStakerBlockDelay
 	diffV2MigrationStakerTarget     = params.DiffV2MigrationStakerTarget
 )
-
-type timeTargetV2 struct {
-	min uint64
-	max uint64
-	blockTarget  uint64
-	pHash   common.Hash
-}
-
-type MineTimeTarget interface {
-	// getters
-	getMinTime() uint64
-	getMaxTime() uint64
-	getTarget() uint64
-	getPeriodTarget() interface{}
-
-	// setters
-	setMinTime(uint64)
-	setMaxTime(uint64)
-	setTarget(uint64)
-}
-
-func (t *timeTargetV2) getMinTime() uint64 {
-	return t.min
-}
-
-func (t *timeTargetV2) setMinTime(minTime uint64) {
-	t.min = minTime
-}
-
-func (t *timeTargetV2) getMaxTime() uint64 {
-	return t.max
-}
-
-func (t *timeTargetV2) setMaxTime(maxTime uint64) {
-	t.max = maxTime
-}
-
-func (t *timeTargetV2) getTarget() uint64 {
-	return t.blockTarget
-}
-
-func (t *timeTargetV2) setTarget(target uint64) {
-	t.blockTarget = target
-}
-
-func (t *timeTargetV2) getPeriodTarget() interface{} {
-	return t.pHash
-}
 
 /*
  * Block Time Target Calculation V2
@@ -101,9 +48,9 @@ func (t *timeTargetV2) getPeriodTarget() interface{} {
 here as an early or late target is for difficulty adjustment not the block
 timestamp
 */
-func (e *Energi) calcTimeTargetV2(chain ChainReader, parent *types.Header) *timeTargetV2 {
+func (e *Energi) calcTimeTargetV2(chain ChainReader, parent *types.Header) *timeTarget {
 
-	ret := &timeTargetV2{}
+	ret := &timeTarget{}
 	parentBlockTime := parent.Time // Defines the original parent block time.
 	parentNumber := parent.Number.Uint64()
 
@@ -113,8 +60,6 @@ func (e *Energi) calcTimeTargetV2(chain ChainReader, parent *types.Header) *time
 	// POS-11: Block time restrictions
 	ret.min = parentBlockTime + params.MinBlockGap
 	ret.blockTarget = parentBlockTime + params.TargetBlockGap
-
-	ret.pHash = parent.Hash()
 
 	// Block interval enforcement
 	// TODO: LRU cache here for extra DoS mitigation
@@ -160,12 +105,11 @@ func (e *Energi) calcTimeTargetV2(chain ChainReader, parent *types.Header) *time
  * If the block time is the target time, the difficulty should stay the same
  * If the block time is more than the target time the difficulty must be reduced
  * New Difficulty = Parent Difficulty * (1.0001 ^ Block Time)
- * NB To reduce the possibility of a difficulty collision, a mantissa is added in the calculation
  */
-func calcPoSDifficultyV2(
+func (e *Energi) calcPoSDifficultyV2(
 	newBlockTime uint64,
 	parent *types.Header,
-	timeTarget *timeTargetV2,
+	timeTarget *timeTarget,
 ) *big.Int {
 
 	target := timeTarget.blockTarget
@@ -216,147 +160,4 @@ func calcPoSDifficultyV2(
 		"parent", parent.Difficulty, "new difficulty", difficulty,
 		"block time", newBlockTime, "target time", target)
 	return difficulty
-}
-
-// MineV2 ...
-//
-// PoS V2 miner implementation,
-// NOTE: this function is no longer used as it's functionality is integrated into mine function and
-// the code that was different is chosen in runtime depending on asgard hardfork status
-func (e *Energi) MineV2(
-	chain ChainReader, header *types.Header, stop <-chan struct{},
-) (success bool, err error) {
-
-	type Candidates struct {
-		addr   common.Address
-		weight uint64
-	}
-
-	accounts := e.accountsFn()
-	// if no accounts are found, just pause and wait for the stop signal
-	//
-	// todo: is this what is intended? Is there a case where this value can
-	//  change but this thread is then dead? I think that very likely this
-	//  should be a repeating loop that retries every minute or something like
-	//  this in case an account is created that can be used, while the server is
-	//  running.
-	//  further thoughts: generally it seems like this function returns quite
-	//  quickly so probably this is fine
-	if len(accounts) == 0 {
-		select {
-		case <-stop:
-			return
-		}
-	}
-
-	parent := chain.GetHeader(header.ParentHash, header.Number.Uint64()-1)
-	if parent == nil {
-		err = eth_consensus.ErrUnknownAncestor
-		return
-	}
-
-	blockTarget := e.calcTimeTargetV2(chain, parent)
-	blockTime := blockTarget.min
-
-	// Special case due to expected very large gap between Genesis and Migration
-	if header.IsGen2Migration() && !e.testing {
-		blockTime = e.now()
-	}
-
-	candidates := make([]Candidates, 0, len(accounts))
-	migrationDPoS := false
-
-	for _, a := range accounts {
-
-		if a == params.Energi_MigrationContract {
-			migrationDPoS = true
-		}
-
-		candidate := Candidates{a, 0}
-		candidates = append(candidates, candidate)
-	}
-
-	// A special workaround to obey target time when migration contract is used
-	// for mining to prevent any difficult bombs.
-	if migrationDPoS && !e.testing && header.Number.Uint64() < params.DiffV2MigrationStakerBlockDelay {
-		// Obey block target
-		if blockTime < blockTarget.blockTarget {
-			blockTime = blockTarget.blockTarget
-		}
-
-		// Decrease difficulty, if it got bumped
-		if header.Difficulty.Uint64() > params.DiffV2MigrationStakerTarget {
-			blockTime += params.DiffV2MigrationStakerTimeDelay
-		}
-	}
-
-	// ---
-	for ; ; blockTime++ {
-		select {
-		case <-stop:
-			// Ensure that a shutdown request is handled as fast as possible.
-			return false, nil
-		default:
-			if maxTime := e.now() + params.MaxFutureGap; blockTime > maxTime {
-				// NOTE: it's very important to ignore stop until all variants are tried
-				//       to prevent rogue stakers taking the initiative.
-				log.Trace("PoS miner is sleeping", "seconds", blockTime-maxTime)
-				<-time.After(time.Duration(blockTime-maxTime) * time.Second)
-			}
-		}
-
-		if e.peerCountFn() == 0 {
-			log.Trace("Skipping PoS miner due to missing peers")
-			continue
-		}
-
-		header.Time = blockTime
-		if blockTarget, err = e.posPrepareV2(chain, header, parent); err != nil {
-			return false, err
-		}
-
-		target := new(big.Int).Div(diffv1.Target, header.Difficulty)
-		log.Trace("PoS miner time", "time", blockTime)
-
-		// It could be done once, but then there is a chance to miss blocks.
-		// Some significant algo optimizations are possible, but we start with simplicity.
-		for i := range candidates {
-			candidate := &candidates[i]
-			candidate.weight, err = e.lookupStakeWeight(
-				chain, blockTime, parent, candidate.addr)
-			if err != nil {
-				return false, err
-			}
-		}
-		// Try smaller amounts first
-		sort.Slice(candidates, func(i, j int) bool {
-			return candidates[i].weight < candidates[j].weight
-		})
-
-		// This tries each candidate for each timestamp before progressing the
-		// timestamp. If the reverse order was desired, the block time needs to
-		// be saved and reset here. Since older is better, this is probably the
-		// better sequence to work in.
-		for _, candidate := range candidates {
-
-			if candidate.weight < 1 {
-				continue
-			}
-
-			// log.Trace("PoS stake candidate", "addr", candidate.addr, "weight", candidate.weight)
-			header.Coinbase = candidate.addr
-			poshash, usedWeight := e.calcPoSHash(header, target, candidate.weight)
-			nonceCap := e.GetMinerNonceCap()
-
-			if nonceCap != 0 && e.nonceCap < usedWeight {
-				continue
-			} else if poshash != nil {
-				log.Trace("PoS stake", "addr", candidate.addr, "weight", candidate.weight, "usedWeight", usedWeight)
-				header.Nonce = types.EncodeNonce(usedWeight)
-				return true, nil
-			}
-		}
-	}
-	// this doesn't need to be strictly stated but this is a long function
-	return success, err
 }
