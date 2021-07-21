494d9490af16be1dd708e11ad6e5bf96f9b5b19f
Ryan Lucchese <ryan@energi.team>
2021-07-20 22:17:36 -0600

    updates to ema test data

--- energi/consensus/intervalgen/main.go
+++ energi/consensus/intervalgen/main.go
@@ -6,10 +6,11 @@ import (
 	"math/rand"
 
 	"energi.world/core/gen3/energi/params"
+	"energi.world/core/gen3/energi/consensus"
 )
 
 func main() {
-	const sampleNum = 60
+	const sampleNum = 1440
 	// This will generate a consistent set of random values
 	rand.Seed(32)
 	// // This will generate an always changing set of random values
@@ -18,16 +19,26 @@ func main() {
 	samples := make([]uint64, sampleNum)
 	for i := range samples {
 		samples[i] = uint64(int64(params.MinBlockGap)+
-				rand.Int63n(int64(params.TargetBlockGap)))
+				rand.Int63n(int64(params.TargetBlockGap+1)))
 	}
+
+	// calculate the EMA from samples
+	samplesEMA := consensus.CalculateBlockTimeEMA(samples)
 	output := `package consensus
 	
 //go:generate go run ./intervalgen/.
 
-var emaSamples = []uint64{
+const emaSamples = []uint64{
 `
 	for i := range samples {
 		output += fmt.Sprint("\t", samples[i], ",\n")
+	}
+	output += `}
+
+const emaSamplesExpected = []uint64{
+`
+	for i := range samplesEMA {
+		output += fmt.Sprint("\t", samplesEMA[i], ",\n")
 	}
 	output += "}\n"
 	ioutil.WriteFile("posv2emasamples_test.go", []byte(output), 0660)
