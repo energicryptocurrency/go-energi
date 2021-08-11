9d8f6238c761e0d7fbea1b95791fd848c9d34d3f
Loki Taljaard <loki.taljaard@energi.team>
2021-08-12 00:14:54 +0200

    Return false nill when exceptions are filtered

--- energi/consensus/engine.go
+++ energi/consensus/engine.go
@@ -557,7 +557,7 @@ func (e *Energi) hardforkIsActive(
 	err = e.hardforkAbi.Unpack(ret, "get", output)
 	if err != nil  {
 		if strings.Contains(err.Error(), "no such hard fork") || strings.Contains(err.Error(), "unmarshalling empty output"){
-			return false, err
+			return false, nil
 		} else{
 			log.Error("Failed to unpack returned hf", "err", err)
 		}
