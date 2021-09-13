a427fdb973b47b7c528d97be0d270b50fdac99e2
Agustin nanni <agustin.nanni97@gmail.com>
2021-09-13 10:50:42 -0300

    moved util to test file

--- energi/consensus/posv2_test.go
+++ energi/consensus/posv2_test.go
@@ -17,8 +17,11 @@
 package consensus
 
 import (
+	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"math/big"
+	"os"
 	"testing"
 
 	"energi.world/core/gen3/common"
@@ -491,4 +494,32 @@ func TestPoSDiffV2(t *testing.T) {
 		res := CalcPoSDifficultyV2(tc.Time, parent, tt).Int64()
 		assert.Equal(t, tc.Result, res, "TC %v", i)
 	}
+}
+
+type PoSDiffV2TestCase struct {
+	Time       uint64
+	Parent     int64
+	Drift      int64
+	Integral   int64
+	Derivative int64
+	Result     int64
+}
+
+func readJsonPoSV2TestCases() (result []PoSDiffV2TestCase) {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := ioutil.ReadFile(dir + "/intervalgen/PoSV2_test_cases.json")
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		panic(err)
+	}
+
+	return
 }
