cc234df190a43ab15c11ab5f4e8385cf3b1b08df
shota.silagadze <shota.silagadze@energi.team>
2021-10-05 10:43:03 +0400

    log trx

--- energi/contracts/test/CheckpointRegistryV3.spec.js
+++ energi/contracts/test/CheckpointRegistryV3.spec.js
@@ -120,9 +120,9 @@ contract("CheckpointRegistryV3", async accounts => {
 
         it('should signer have correct balance', async () => {
             try {
-                web3.eth.sendTransaction({to:'0x2d0bc327d0843caf6fd9ae1efab0bf7196fc2fc8', from: accounts[0], value: toWei('5000000', 'ether')}).then((value) => {
-                  assert.equal(value,true,"bla");
-                  // expected output: "Success!"
+                web3.eth.sendTransaction({to:'0x2d0bc327d0843caf6fd9ae1efab0bf7196fc2fc8', from: accounts[0], value: toWei('5000000', 'ether')}).then((value) =>
+                    assert.equal(value,true,"bla");
+                    // expected output: "Success!"
                 });
             } catch (e) {
                 assert.match(e.message, /Invalid signature length/);
