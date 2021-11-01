e74271c050125838e8b075272b56065fa6b1f3b7
shota.silagadze <shota.silagadze@energi.team>
2021-11-01 13:40:04 +0400

    new treasury v2 migration file

---
+++ energi/contracts/migrations/8_treasury_v2.js
@@ -0,0 +1,41 @@
+// Copyright 2021 The Energi Core Authors
+// This file is part of the Energi Core library.
+//
+// The Energi Core library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Energi Core library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Energi Core library. If not, see <http://www.gnu.org/licenses/>.
+
+'use strict';
+
+const TreasuryV2 = artifacts.require('TreasuryV2');
+const common = require('../test/common');
+
+module.exports = async function(deployer, network) {
+    try {
+        var treasury_proxy = '0x0000000000000000000000000000000000000301';
+        var mn_registry_proxy = '0x0000000000000000000000000000000000000302';
+        console.log("Deploying TreasuryV2 to " + network);
+
+        if (network === "mainnet" || network === "testnet") {
+          // since this uses GovernedContractAutoProxy, make sure we capture the new proxy address
+          await deployer.deploy(TreasuryV2, treasury_proxy, mn_registry_proxy, common.superblock_cycles);
+          var instance = await TreasuryV2.deployed();
+          var proxyAddress = await instance.proxy();
+          console.log("   > proxy address:       " + proxyAddress);
+        } else {
+            return;
+        }
+    } catch (e) {
+        console.dir(e);
+        throw e;
+    }
+};
