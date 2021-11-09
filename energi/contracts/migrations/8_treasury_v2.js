5db12f4f39bb2843d5a655a73524c337c2a3e8b5
shota.silagadze <shota.silagadze@energi.team>
2021-11-09 12:17:51 +0400

    fix superblock cycle

--- energi/contracts/migrations/8_treasury_v2.js
+++ energi/contracts/migrations/8_treasury_v2.js
@@ -21,13 +21,19 @@ const common = require('../test/common');
 
 module.exports = async function(deployer, network) {
     try {
+        console.log("Deploying TreasuryV2 to " + network);
         const treasury_proxy = '0x0000000000000000000000000000000000000301';
         const mn_registry_proxy = '0x0000000000000000000000000000000000000302';
-        console.log("Deploying TreasuryV2 to " + network);
+        const mainnet_superblock_cycle = 60 * 24 * 14
+        const testnet_superblock_cycle = 60 * 24
 
         if (network === "mainnet" || network === "testnet") {
             // since this uses GovernedContractAutoProxy, make sure we capture the new proxy address
-            await deployer.deploy(TreasuryV2, treasury_proxy, mn_registry_proxy, common.superblock_cycles);
+            if (network === "mainnet") {
+              await deployer.deploy(TreasuryV2, treasury_proxy, mn_registry_proxy, mainnet_superblock_cycle);
+            } else {
+              await deployer.deploy(TreasuryV2, treasury_proxy, mn_registry_proxy, testnet_superblock_cycle);
+            }
             const instance = await TreasuryV2.deployed();
             const proxyAddress = await instance.proxy();
             console.log("   > proxy address:       " + proxyAddress);
