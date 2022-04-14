705ef6cf43cfa7aa168dac729800bdcd7550e610
shota.silagadze <shota.silagadze@energi.team>
2022-04-14 12:34:30 +0400

    remove duplicate code

--- energi/consensus/reward.go
+++ energi/consensus/reward.go
@@ -153,14 +153,14 @@ func (e *Energi) processFeeReward(
 	reward := (header.GasUsed * uint64(energi_params.StakerReward)) / Hundred
 
 	// reward staker
-	stakerRewardTx, stakerRewardReceipts, err := e.processStakerFeeReward(chain, header, statedb, txs, reward)
+	stakerRewardTx, stakerRewardReceipts, err := e.sendFeeReward(chain, header, statedb, txs, header.Coinbase, reward)
 	if err != nil {
 		log.Error("Fail to reward staker", "err", err)
 		return txs, receipts, err
 	}
 
 	// mint remaining fees
-	mintRewardTx, mintReceipts, err := e.mintFees(chain, header, statedb, txs, header.GasUsed-reward)
+	mintRewardTx, mintReceipts, err := e.sendFeeReward(chain, header, statedb, txs, common.Address{}, header.GasUsed-reward)
 	if err != nil {
 		log.Error("Fail to mint fees", "err", err)
 		return txs, receipts, err
@@ -169,66 +169,19 @@ func (e *Energi) processFeeReward(
 	return append(txs, stakerRewardTx, mintRewardTx), append(receipts, stakerRewardReceipts, mintReceipts), nil
 }
 
-func (e *Energi) processStakerFeeReward(
+func (e *Energi) sendFeeReward(
 	chain ChainReader,
 	header *types.Header,
 	statedb *state.StateDB,
 	txs types.Transactions,
+	address common.Address,
 	reward uint64,
 ) (*types.Transaction, *types.Receipt, error) {
 	systemFaucet := e.systemFaucet
 	// Reward staker
 	tx := types.NewTransaction(
 		statedb.GetNonce(systemFaucet),
-		header.Coinbase,
-		new(big.Int).SetUint64(reward),
-		e.xferGas,
-		common.Big0,
-		nil)
-	tx = tx.WithConsensusSender(systemFaucet)
-	statedb.Prepare(tx.Hash(), header.Hash(), len(txs))
-
-	msg, err := tx.AsMessage(&ConsensusSigner{})
-	if err != nil {
-		log.Error("Failed in BlockReward AsMessage()", "err", err)
-		return nil, nil, err
-	}
-
-	evm := e.createEVM(msg, chain, header, statedb)
-	gp := core.GasPool(msg.Gas())
-
-	// apply transaction
-	_, usedGas, failed, err := core.ApplyMessage(evm, msg, &gp)
-	if err != nil {
-		log.Error("Failed in reward() call", "err", err)
-		return nil, nil, err
-	}
-
-	// NOTE: it should be Byzantium finalization here...
-	root := statedb.IntermediateRoot(chain.Config().IsEIP158(header.Number))
-	receipt := types.NewReceipt(root.Bytes(), failed, header.GasUsed)
-	receipt.TxHash = tx.Hash()
-	receipt.GasUsed = usedGas
-	receipt.Logs = statedb.GetLogs(tx.Hash())
-	receipt.Bloom = types.CreateBloom(types.Receipts{receipt})
-
-	log.Trace("Staker reward", "reward", reward, "gas", usedGas)
-
-	return tx, receipt, nil
-}
-
-func (e *Energi) mintFees(
-	chain ChainReader,
-	header *types.Header,
-	statedb *state.StateDB,
-	txs types.Transactions,
-	reward uint64,
-) (*types.Transaction, *types.Receipt, error) {
-	systemFaucet := e.systemFaucet
-	// Reward staker
-	tx := types.NewTransaction(
-		statedb.GetNonce(systemFaucet),
-		common.Address{},
+		address,
 		new(big.Int).SetUint64(reward),
 		e.xferGas,
 		common.Big0,
