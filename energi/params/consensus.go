5437fc3ba505c562ae766ee3a7f220f44ffd1101
shotasilagadze <silagadzeshota@gmail.com>
2023-01-17 18:29:40 +0400

    update gain parameter value

--- energi/params/consensus.go
+++ energi/params/consensus.go
@@ -60,5 +60,5 @@ const (
 	StakeCheckDepth = 0
 
 	// GainBanana parameter is part of difficulty adjustment algorithm that corresponds to the speed of adjustment of the difficulty
-	GainBanana = 20000
+	GainBanana = 15000
 )
