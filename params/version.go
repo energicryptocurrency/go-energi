befec582700f794171278f9e0b811f20b12f0495
Ryan Lucchese <ryan@energi.team>
2023-02-25 08:41:00 -0700

    version update

--- params/version.go
+++ params/version.go
@@ -23,7 +23,7 @@ const (
 	VersionMajor = 1        // Major version component of the current release
 	VersionMinor = 1        // Minor version component of the current release
 	VersionPatch = 4        // Patch version component of the current release
-	VersionMeta  = "hotfix-test" // Version metadata to append to the version string
+	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
